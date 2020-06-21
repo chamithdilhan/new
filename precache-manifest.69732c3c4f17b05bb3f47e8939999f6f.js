d725603e17afac4e5f1c4f10d1316636ef8d46ce
chamithdilhan <thilinachamith1997@gmail.com>
2020-06-21 12:42:48 +0530

    Updates

---
+++ precache-manifest.69732c3c4f17b05bb3f47e8939999f6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8e6a14e34cf138b0d4cf1d3ccc4218",
+    "url": "/reacttodo.sadithapineapple.lk/index.html"
+  },
+  {
+    "revision": "db1336c1dbb9aa3b5602",
+    "url": "/reacttodo.sadithapineapple.lk/static/css/main.a97be95d.chunk.css"
+  },
+  {
+    "revision": "bd0c97b7a831b9122b2d",
+    "url": "/reacttodo.sadithapineapple.lk/static/js/2.0512a152.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttodo.sadithapineapple.lk/static/js/2.0512a152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db1336c1dbb9aa3b5602",
+    "url": "/reacttodo.sadithapineapple.lk/static/js/main.dffe6df9.chunk.js"
+  },
+  {
+    "revision": "d319bd01a3843d0255c0",
+    "url": "/reacttodo.sadithapineapple.lk/static/js/runtime-main.6592234a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacttodo.sadithapineapple.lk/static/media/logo.5d5d9eef.svg"
+  }
+]);
